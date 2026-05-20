b120843824e477fd2920a47b993e813a5d3d9299
Arturo Cornes <bame36@gmail.com>
2026-05-20 20:11:56 -0300

    Creacion de las 3 reglas ejemplo

--- analisis_pyDatalog.py
+++ analisis_pyDatalog.py
@@ -4,6 +4,7 @@ from hechos_datalog import *
 #cargar hechos en memoria
 cargar_hechos()
 
+#region Proveedor Frecuente
 #crear términos nuevos
 pyDatalog.create_terms('X, P, O, N, C,compra_grande,proveedor_frecuente, cantidad_adjudicaciones')
 
@@ -14,7 +15,9 @@ pyDatalog.create_terms('X, P, O, N, C,compra_grande,proveedor_frecuente, cantida
 # regla
 proveedor_frecuente(P) <= (cantidad_adjudicaciones[P] > 10)
 
+#endregion
 
+#region Alta concentracion
 
 pyDatalog.create_terms(
     '''
@@ -26,7 +29,48 @@ pyDatalog.create_terms(
     '''
 )
 
+#funcion
+(adjudicaciones_org[O] == len_(X)) <= (organismo_de[X] == O)
 
-# consultas
 
-#print(proveedor_frecuente(P))
+(adjudicaciones_proveedor_org[O, P] == len_(X)) <= (
+    (organismo_de[X] == O) &
+    (proveedor_de[X] == P) &
+    (P != 'NAN')
+)
+
+#regla
+alta_concentracion(O, P) <= (
+    (adjudicaciones_proveedor_org[O, P] * 100)
+    / adjudicaciones_org[O] > 40
+)
+
+#consulta
+#print(alta_concentracion(O, P))
+
+#endregion
+
+#region Adjudicacion Repetida
+pyDatalog.create_terms(
+    '''
+    X, O, P, C,
+    adjudicaciones_proveedor_org,
+    adjudicacion_repetida
+    '''
+)
+
+#funcion
+(adjudicaciones_proveedor_org[O, P] == len_(X)) <= (
+    (organismo_de[X] == O) &
+    (proveedor_de[X] == P) &
+    (P != 'NAN')
+)
+
+#regla
+adjudicacion_repetida(O, P) <= (adjudicaciones_proveedor_org[O, P] > 3)
+
+#consulta
+#print(adjudicaciones_proveedor_org[O, P] == C)
+
+#endregion
+
